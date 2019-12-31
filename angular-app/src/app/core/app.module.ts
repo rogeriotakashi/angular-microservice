9ebeaacb5a90ada5cd63d7a9647428b7f0927a32
rogeriotakashi <rogeriotakashi@hotmail.com>
2019-12-31 15:42:59 -0300

    Refactor: imports paths

--- angular-app/src/app/core/app.module.ts
+++ angular-app/src/app/core/app.module.ts
@@ -3,9 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginFormComponent } from '../modules/login-form/login-form.component';
-import { NavBarComponent } from './header/nav-bar/nav-bar.component';
-import { HomeComponent } from '../modules/home/home.component';
+import { LoginFormComponent } from '@component/login-form/login-form.component';
+import { HomeComponent } from '@component/home/home.component';
+import { NavBarComponent } from '@core/header/nav-bar/nav-bar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
--- angular-app/src/app/modules/home/home.component.spec.ts
+++ angular-app/src/app/modules/home/home.component.spec.ts
@@ -1,6 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { HomeComponent } from './home.component';
+import { HomeComponent } from '@component/home/home.component';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
